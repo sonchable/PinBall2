b81d43bd2ccb0419d0ca5080c6e457c24a5c51ce
sonchable <sonchoable@yahoo.co.jp>
2017-06-26 19:51:41 +0900

    no message

---
+++ Assets/scoreScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreScript : MonoBehaviour {
+
+	public Text scoreText;
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		score = 0;
+		SetScore ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter( Collision other){
+
+		string yourTag = other.gameObject.tag;
+
+		if (yourTag == "SmallStarTag") {
+			score += 10;
+		} else if (yourTag == "LargeStarTag") {
+			score += 30;
+		} else if (yourTag == "SmallCloudTag") {
+			score += 15;
+		} else if (yourTag == "LargeCloudTag") {
+			score += 50;
+		}
+
+		SetScore ();
+	}
+
+	void SetScore (){
+		scoreText.text = string.Format ("Score:{0}", score);
+	}
+
+}
